27e078f6bc2f0239a150691b1a0925f5656b8b78
Tim Duckett <tim.duckett@bcgdv.com>
2015-11-05 08:52:05 +0100

    Initial Commit

---
+++ Trackr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trackr
+//
+//  Created by Tim Duckett on 05/11/15.
+//  Copyright © 2015 Tim Duckett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
